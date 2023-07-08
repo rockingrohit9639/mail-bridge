0751a99dd3820fa762e8b8df7bf274dcac7a582d
Rohit Saini <rohit@prodios.com>
2023-07-08 10:31:41 +0530

    💡 feat(init): initialize turborepo with eslint and prettier

---
+++ packages/configs/prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  endOfLine: 'auto',
+}
+
+module.exports = config
