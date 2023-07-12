fa94113b0be9f6927878db36f0ad65c7ece2523b
Rohit Saini <rohit@prodios.com>
2023-07-12 19:52:39 +0530

    💡 feat(docusaurus): add app for documentation using docusaurus

---
+++ apps/docs/docusaurus.config.js
@@ -0,0 +1,67 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Docs | Mail Bridge',
+  tagline: 'Simplify Your Email Sending Process',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+
+  organizationName: 'rockingrohit9639', // Usually your GitHub org/user name.
+  projectName: 'mail-bridge', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Mail Bridge',
+        logo: {
+          alt: 'Mail Bridge',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Mail Bridge, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
+
+module.exports = config
